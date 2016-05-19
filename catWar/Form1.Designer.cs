15415044fd59ca7b0ea6b7068572490fd43e17f9
khtp91113 <khtp91113@mail.ncku.edu.tw>
2016-05-19 12:02:36 +0800

    menu v2

--- catWar/Form1.Designer.cs
+++ catWar/Form1.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 83);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "普通模式";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(69, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(273, 83);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "無盡模式";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(69, 387);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(273, 83);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "離開";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(686, 419);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(684, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(686, 474);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(185, 56);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "遊戲開始";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(1200, 700);
+            this.MinimumSize = new System.Drawing.Size(1200, 700);
+            this.Name = "Form1";
+            this.Text = "選單頁面";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- catWar/Form1.cs
+++ catWar/Form1.cs
@@ -12,9 +12,55 @@ namespace catWar
 {
     public partial class Form1 : Form
     {
+        static public Form1 f1;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Hide();
+            comboBox1.Hide();
+            button4.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Show();
+            label1.Text = "普通模式\n說明: ...";
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Show();
+            button4.Show();
+            label1.Text = "普通模式\n說明: ...\n請選擇難度";
+            String[] difficulty = new String[] {"簡單","普通","困難","地獄"};
+            comboBox1.Items.AddRange(difficulty);
+            comboBox1.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f1 = this;
+            this.Hide();
+            f2.Show();
+            label1.Hide();
+            button4.Hide();
+            comboBox1.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Hide();
+            label1.Show();
+            button4.Show();
+            label1.Text = "無盡模式\n說明: ...";
+        }
+
     }
 }
